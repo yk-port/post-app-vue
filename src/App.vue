c38c6e42e5e10fa97ccefc482f8210f8d7ccf51b
Tatsuya <yokokura0413@gmail.com>
2021-03-20 10:03:53 +0900

    add: NavBarコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app dense>
-      <v-toolbar-title>PostApp</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text>
-        <router-link :to="{ name: 'events' }">LIST</router-link>
-      </v-btn>
-      <v-btn text>
-        <router-link :to="{ name: 'events-new' }">NEW</router-link>
-      </v-btn>
-      <v-btn text>
-        <router-link :to="{ name: 'mypage', params: { username: 'tatsuya' } }"
-          >ACCOUNT</router-link
-        >
-      </v-btn>
-    </v-app-bar>
-
+    <NavBar />
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
 
-<script></script>
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style lang="scss">
 a {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar app dense>
+    <v-toolbar-title>PostApp</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text>
+      <router-link :to="{ name: 'events' }">LIST</router-link>
+    </v-btn>
+    <v-btn text>
+      <router-link :to="{ name: 'events-new' }">NEW</router-link>
+    </v-btn>
+    <v-btn text>
+      <router-link :to="{ name: 'mypage', params: { username: 'tatsuya' } }"
+        >ACCOUNT</router-link
+      >
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
