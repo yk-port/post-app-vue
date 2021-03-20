9d5173965466ec804a862b805b5801273caed8c9
Tatsuya <yokokura0413@gmail.com>
2021-03-20 11:08:16 +0900

    add: GoodIconコンポーネント

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,31 +1,36 @@
 <template>
-  <router-link :to="{ name: 'events-show', params: { id: '1' } }">
-    <v-card class="mx-auto" max-width="344">
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">@{{ event.time }} on {{ event.date }}</div>
-          <v-list-item-title class="headline mb-1">
-            {{ event.title }}
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            {{ event.attendees.length }} attending
-          </v-list-item-subtitle>
-        </v-list-item-content>
+  <v-card class="mx-auto" max-width="344">
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">@{{ event.time }} on {{ event.date }}</div>
+        <v-list-item-title class="headline mb-1">
+          {{ event.title }}
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          {{ event.attendees.length }} attending
+        </v-list-item-subtitle>
+      </v-list-item-content>
 
-        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
-      </v-list-item>
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
 
-      <v-card-actions>
+    <v-card-actions>
+      <router-link :to="{ name: 'events-show', params: { id: '1' } }">
         <v-btn outlined rounded text>
-          Button
+          more
         </v-btn>
-      </v-card-actions>
-    </v-card>
-  </router-link>
+      </router-link>
+      <GoodIcon />
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
+import GoodIcon from '@/components/GoodIcon.vue'
 export default {
+  components: {
+    GoodIcon
+  },
   data() {
     return {
       event: {
---
+++ src/components/GoodIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn class="ma-2" text icon color="blue lighten-2">
+    <v-icon>mdi-thumb-up</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
